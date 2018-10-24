517781be4ccc2a46a73e68ea4246980f3c9408d0
Joey Yang <joey.yang1997@hotmail.com>
2018-10-24 18:00:38 -0400

    implemented fleet vector to store all robot objects

--- main.cpp
+++ main.cpp
@@ -1,32 +1,92 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "robot.h"
 
 //namespace: groups of identifiers
 //using namespace std;
 
+/*----function declarations (prototypes)----*/
+
+//pass in vector argument by reference
+//note: vectors take up large amounts of memory
+//rather than creating duplicates of the same vector through passing in by value, pass vector in by reference instead to access the original vector
+void fill_fleet_vector(std::vector<robot>&, int);
+//fills fleet vector with robot class type objects
+//@param1 vector<robot>& - robots of fleet
+//@param2 int - number of robots to deploy
+//@return - none
+
+//pass in vector argument by reference
+void print_fleet_vector(const std::vector<robot>&, int, int);
+//prints robots of fleet
+//@param1 int - number of robots in system
+//@param2 int - maximum number of robots allowed in system
+//@return - none
+
 int main() {
 	int number_of_robots, system_capacity;
-	number_of_robots = 0;
 
-	std::cout << "Enter maximum robot capacity of system: " << std::endl;
+	//initialize vector of robot class type to store fleet of robot objects
+	std::vector<robot> fleet;
+
+	std::cout << "Enter maximum number robots allowed in system: ";
 	std::cin >> system_capacity;
+	std::cout << std::endl;
 
-	std::cout << "Creating robot..." << std::endl;
-	//create robot object Optimus Prime
-	robot Optimus_Prime(0, 2.00, "Autobots, Roll Out!", 'A');
-	number_of_robots++;
+	std::cout << "Enter number of robots to deploy: ";
+	std::cin >> number_of_robots;
+	std::cout << std::endl;
 
-	std::cout << "Robot created." << std::endl <<
-		"ID: " << Optimus_Prime.get_ID() << std::endl <<
-		"Speed: " << Optimus_Prime.get_speed() << std::endl <<
-		"Message: " << Optimus_Prime.get_message() << std::endl <<
-		"Grade: " << Optimus_Prime.get_grade() << std::endl <<
-		"Utilization: " << Optimus_Prime.calculate_utilization(number_of_robots, system_capacity) << "%" <<std::endl;
+	//deploy robot fleet
+	fill_fleet_vector(fleet, number_of_robots);
 
-	std::cout << "System capacity: " << system_capacity << std::endl <<
-		"Number of robots in system: " << number_of_robots << std::endl;
-	
+	//print robot fleet
+	print_fleet_vector(fleet, number_of_robots, system_capacity);
+
 	return 0;
+}
+
+/*----function definitions----*/
+
+void fill_fleet_vector(std::vector<robot>& fleet, int number_of_robots) {
+	int i;
+	long id;
+	double speed;
+	std::string message;
+	char grade;
+
+	for (i = 0; i < number_of_robots; i++) {
+		std::cout << "Enter robot ID: ";
+		std::cin >> id;
+		std::cout << "Enter robot speed: ";
+		std::cin >> speed;
+		std::cout << "Enter robot message: ";
+		std::cin >> message;
+		std::cout << "Enter robot grade: ";
+		std::cin >> grade;
+
+		//create robot object
+		robot new_robot(id, speed, message, grade);
+
+		//add robot object to fleet vector
+		fleet.push_back(new_robot);
+
+		std::cout << std::endl;
+	}
+}
+
+void print_fleet_vector(const std::vector<robot>& fleet, int number_of_robots, int system_capacity) {
+	int i;
+	int fleet_size = fleet.size();
+
+	for (i = 0; i < fleet_size; i++) {
+		std::cout << "Robot ID: " << fleet[i].get_ID() << std::endl;
+		std::cout << "Robot speed: " << fleet[i].get_speed() << std::endl;
+		std::cout << "Robot message: " << fleet[i].get_message() << std::endl;
+		std::cout << "Robot grade: " << fleet[i].get_grade() << std::endl;
+		std::cout << "Robot fleet utilization: " << fleet[i].calculate_utilization(number_of_robots, system_capacity) << std::endl;
+		std::cout << std::endl;
+	}
 }
