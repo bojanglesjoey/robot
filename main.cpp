5b679e9e440466e60312b90fa6fa348cc006a246
Joey Yang <joey.yang1997@hotmail.com>
2018-11-21 21:28:11 -0500

    added variable names to function prototypes for readability

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 //using namespace std;
 
 //mission log file paths
-const std::string missions_inpath = "C:/Users/jyang/Documents/missions.csv";
+const std::string missions_inpath = "/home/joey/Documents/projects/test/missions.csv";
 const std::string missions_outpath = " ";
 
 /*----function declarations (prototypes)----*/
@@ -19,7 +19,7 @@ const std::string missions_outpath = " ";
 //rather than creating duplicates of the same vector through passing in by value
 //pass the vector in by reference instead to access the original vector
 //this will also change the original vector passed in to be used later, without needing to return the changed vector
-void fill_fleet_vector(std::vector<robot>&, int);
+void fill_fleet_vector(std::vector<robot>& fleet, int number_of_robots);
 //fills the fleet vector with robot class type objects
 //@param1 vector<robot>& - robots of fleet
 //@param2 int - number of robots deployed
@@ -28,20 +28,20 @@ void fill_fleet_vector(std::vector<robot>&, int);
 //pass in vector argument by const-reference
 //this function will not change the original vector passed in, therefore use const
 //passing in by reference is still required for vector for efficiency, compared to passing in by value
-void print_fleet_vector(const std::vector<robot>&, int, int);
+void print_fleet_vector(const std::vector<robot>& fleet, int number_of_robots, int optimal_fleet_size);
 //prints robots of the fleet
 //@param1 vector<robot>& - robots of fleet
 //@param2 int - number of robots deployed
 //@param3 int - optimal number of robots for system
 //@return - none
 
-int calculate_optimal_fleet_size(int, int);
+int calculate_optimal_fleet_size(int minimum_fleet_size, int system_capacity);
 //randomly calculates the optimal number of robots for the system
 //based on the minimum number of robots allowed in system and the maximum number of robots allowed in system
 //@param1 int - minimum number of robots allowed in system
 //@param2 int - maximum number of robots allowed in system
 
-float calculate_fleet_utilization(int, int);
+float calculate_fleet_utilization(int number_of_robots, int optimal_fleet_size);
 //calculates new fleet utilization based on number of robots deployed and the optimal number of robots for system
 //@param1 int - number of robots deployed
 //@param2 int - optimal number of robots for system
@@ -78,13 +78,13 @@ int main(int argc, char* argv[]) {
 	std::cin >> number_of_robots;
 
 	//check to make sure number of robots does not exceed system capacity
-	if (number_of_robots > system_capacity) {
+	if(number_of_robots > system_capacity) {
 		std::cout << "Error: number of robots to deploy exceeds the maximum number of robots allowed in system." << std::endl;
 		//ends program (conditional statement must be in main function)
 		return -1;
 	}
 	//check to make sure robots are actually deployed
-	if (number_of_robots == 0) {
+	if(number_of_robots == 0) {
 		std::cout << "Error: no robots to be deployed." << std::endl;
 		return -1;
 	}
@@ -117,7 +117,7 @@ void fill_fleet_vector(std::vector<robot>& fleet, int number_of_robots) {
 	std::string message;
 	char grade;
 
-	for (i = 0; i < number_of_robots; i++) {
+	for(i = 0; i < number_of_robots; i++) {
 		std::cout << "Enter robot ID: ";
 		std::cin >> id;
 		std::cout << "Enter robot speed: ";
@@ -141,12 +141,12 @@ void print_fleet_vector(const std::vector<robot>& fleet, int number_of_robots, i
 	int i;
 	int fleet_size = fleet.size();
 
-	if (fleet_size == 0) {
+	if(fleet_size == 0) {
 		std::cout << "There are no robots in the fleet." << std::endl;
 	}
 
 	else {
-		for (i = 0; i < fleet_size; i++) {
+		for(i = 0; i < fleet_size; i++) {
 			std::cout << "Robot ID: " << fleet[i].get_ID() << std::endl;
 			std::cout << "Robot speed: " << fleet[i].get_speed() << std::endl;
 			std::cout << "Robot message: " << fleet[i].get_message() << std::endl;
@@ -164,7 +164,7 @@ int calculate_optimal_fleet_size(int minimum_fleet_size, int system_capacity) {
 float calculate_fleet_utilization(int number_of_robots, int optimal_fleet_size) {
 	float fleet_utilization;
 
-	if (number_of_robots <= optimal_fleet_size) {
+	if(number_of_robots <= optimal_fleet_size) {
 		return fleet_utilization = 100;
 	}
 
