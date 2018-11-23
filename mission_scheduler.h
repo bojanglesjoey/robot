7faae04113a5bd3b438dedd07d5b020cf2d5ee4a
Joey Yang <joey.yang1997@hotmail.com>
2018-11-23 16:23:12 -0500

    added struct member initialization list

--- mission_scheduler.h
+++ mission_scheduler.h
@@ -1,6 +1,8 @@
 /*
 	File: mission_scheduler.h
 
+	Description: mission scheduler header file; contains function declarations.
+
 	Author: Joey Yang
 */
 
@@ -13,7 +15,15 @@
 
 class schedule {
 public:
+	//constructor
 	schedule();
+
+	/* rule of 3 */
+	schedule(const schedule& other_schedule);
+	schedule& operator=(const schedule& other_schedule);
+	~schedule();
+
+	/* class methods */
 	bool schedule_empty() const;
 	int get_schedule_size() const;
 	void append_new_mission(const int ID, const double distance);
@@ -23,23 +33,15 @@ public:
 	void print_schedule() const;
 	void load_missions(std::ifstream& missions_infile);
 	//void schedule_missions();
-	~schedule();
+	
 
 private:
 	struct mission {
 		int ID;
 		double distance;
 		mission* next;
-		mission() {
-			ID = 0;
-			distance = 0;
-			next = nullptr;
-		}
-		mission(const int init_ID, const double init_distance) {
-			ID = init_ID;
-			distance = init_distance;
-			next = nullptr;
-		}
+		mission() : ID(0), distance(0), next(nullptr) {}
+		mission(const int init_ID, const double init_distance) : ID(init_ID), distance(init_distance), next(nullptr) {}
 	};
 
 	mission* head;
