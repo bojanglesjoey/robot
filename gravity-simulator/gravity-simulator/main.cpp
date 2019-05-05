f81906500eed4a363f5a54ad37d1280cbd352e94
Joey Yang <yangj30@mcmaster.ca>
2019-05-05 01:45:29 -0400

    moving celestial by time

--- gravity-simulator/gravity-simulator/main.cpp
+++ gravity-simulator/gravity-simulator/main.cpp
@@ -1,22 +1,34 @@
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
 #include "celestial.h"
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(400, 400), "Window");
-	
-	celestial::Celestial celestial(sf::Vector2f(10.f, 10.f), 30);
+	sf::Clock clock;
+
+	celestial::Celestial celestial(sf::Vector2f(25.f, 25.f), 100);
 	celestial.setFillColor(sf::Color::Green);
-	celestial.setOutlineThickness(10.f);
-	celestial.setOutlineColor(sf::Color(250, 150, 100));
+	celestial.setPosition(200.f, 200.f);
 
 	while (window.isOpen())
 	{
 		sf::Event event;
+		sf::Time elapsed = clock.getElapsedTime();
+		
+
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+		}
+
+		if (elapsed.asSeconds() > 1)
+		{
+			sf::Vector2f current_position = celestial.getPosition();
+			celestial.move(30.f, 30.f);
+			clock.restart();
 		}
 
 		window.clear();
